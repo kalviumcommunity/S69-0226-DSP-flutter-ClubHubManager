1f5e6da2434905b3c85a165cb4355210b77667a5
Shanmugapriya222 <shanmugapriyab211@gmail.com>
2026-02-18 14:09:38 +0530

    ...

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,11 +27,10 @@ android {
         applicationId = "com.example.club_hub"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        multiDexEnabled = true
     }
 
     buildTypes {
